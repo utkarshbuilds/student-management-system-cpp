3ebe79e49fa0cfe829585d564aadfde273f467c9
Utkarsh Raj <utkarshraj1825@gmail.com>
2026-05-03 19:25:23 +0530

    Removed input handling from the student.cpp

--- student.cpp
+++ student.cpp
@@ -2,18 +2,6 @@
 #include <iostream>
 using namespace std;
 
-void Student::input()
-{
-    cout << "Enter id: ";
-    cin  >> id;
-
-    cout << "Enter name: ";
-    cin >> name;
-
-    cout << "Enter age: ";
-    cin >> age;
-}
-
 void Student::display()
 {
     cout << "ID: " << id << endl;
